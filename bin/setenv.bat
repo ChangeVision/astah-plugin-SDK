9f9570fb574ecb59203fa0d8b8907ab5c152619c
Hiroki Kondo <kompiro@gmail.com>
2013-02-19 14:03:31 +0900

    refs #3039 help コマンドを追加し、デフォルトバージョンを設定するようにしました

--- bin/setenv.bat
+++ bin/setenv.bat
@@ -3,3 +3,7 @@ if NOT DEFINED ASDK_HOME set ASDK_HOME=%~dp0..
 set MAVEN_HOME=%ASDK_HOME%
 set M2_HOME=%ASDK_HOME%
 set MAVEN_BAT=%~dp0astah-mvn.bat
+set GROUP_ID="com.change_vision.astah"
+set ARTIFACT_ID="astah-maven-plugin"
+set VERSION="1.0.0"
+set ASTAH_MAVEN_PLUGIN="%GROUP_ID%:%ARTIFACT_ID%:%VERSION%"
--- bin/setenv.sh
+++ bin/setenv.sh
@@ -3,3 +3,7 @@ export ASDK_HOME="$ASDK_HOME"
 export MAVEN_HOME="$ASDK_HOME"
 export M2_HOME="$ASDK_HOME"
 export MAVEN_SH="$ASDK_HOME"/bin/astah-mvn
+GROUP_ID="com.change_vision.astah"
+ARTIFACT_ID="astah-maven-plugin"
+VERSION="1.0.0"
+export ASTAH_MAVEN_PLUGIN="$GROUP_ID:$ARTIFACT_ID:$VERSION"
