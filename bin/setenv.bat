58bf9da485b140fc6954d2dcd19a274200058c2b
myamamoto <m.yamamoto@change-vision.com>
2018-09-04 17:14:07 +0900

    astah-maven-pluginを1.0.2に変更

--- bin/setenv.bat
+++ bin/setenv.bat
@@ -5,5 +5,5 @@ set M2_HOME=%ASDK_HOME%
 set MAVEN_BAT=%~dp0astah-mvn.bat
 set GROUP_ID="com.change_vision.astah"
 set ARTIFACT_ID="astah-maven-plugin"
-set VERSION="1.0.1"
+set VERSION="1.0.2"
 set ASTAH_MAVEN_PLUGIN="%GROUP_ID%:%ARTIFACT_ID%:%VERSION%"
--- bin/setenv.sh
+++ bin/setenv.sh
@@ -5,5 +5,5 @@ export M2_HOME="$ASDK_HOME"
 export MAVEN_SH="$ASDK_HOME"/bin/astah-mvn
 GROUP_ID="com.change_vision.astah"
 ARTIFACT_ID="astah-maven-plugin"
-VERSION="1.0.1"
+VERSION="1.0.2"
 export ASTAH_MAVEN_PLUGIN="$GROUP_ID:$ARTIFACT_ID:$VERSION"
